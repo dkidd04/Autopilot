692a061db2e6a0f3cc871d29cf836d9a9c263a6e
Darren Rodgers <darren1.rodgers@citi.com>
2016-08-26 12:12:30 +0100

    Moving default symbol to HOME.L
    Changes to allow easier use of serverMode

--- src/main/java/com/citigroup/liquifi/autopilot/bootstrap/AutoPilotBootstrap.java
+++ src/main/java/com/citigroup/liquifi/autopilot/bootstrap/AutoPilotBootstrap.java
@@ -1,19 +1,20 @@
 package com.citigroup.liquifi.autopilot.bootstrap;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 import javax.swing.UIManager;
 
 import com.citigroup.liquifi.autopilot.controller.TestCaseController;
-import com.citigroup.liquifi.autopilot.bootstrap.AutoPilotBenchMarkSuite;
 import com.citigroup.liquifi.autopilot.controller.ValidationObject;
 import com.citigroup.liquifi.autopilot.logger.AceLogger;
 import com.citigroup.liquifi.util.ClassPathLoader;
 import com.citigroup.liquifi.util.DBUtil;
 
 public class AutoPilotBootstrap {
-	private static AceLogger logger = AceLogger.getLogger("AutoPilotBootstrap");
+	private final static AceLogger logger = AceLogger.getLogger("AutoPilotBootstrap");
 
 	public static void initDB() {
 		logger.info("Loading Template from Database ... ");
@@ -39,162 +40,19 @@ public class AutoPilotBootstrap {
 	 */
 	public static void main(String[] args) {
 		try {
-
-			if (System.getProperty("mode", "Servermode").equalsIgnoreCase(
-					"Guimode")) {
-				logger.info("Launch AutoPilot in Guimode ...");
-				UIManager.setLookAndFeel(UIManager
-						.getSystemLookAndFeelClassName());
+			String mode = System.getProperty("mode", "Servermode").toLowerCase();
+			logger.info("Launch AutoPilot in "+mode+" ...");
+			switch(mode){
+			case "guimode":
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 				new AutoPilotGuiStarter(args);
-
-			} else if (System.getProperty("mode", "Servermode")
-					.equalsIgnoreCase("Benchmarkmode")) {
-				logger.info("Launch AutoPilot in Benchmarkmode ...");
-				initSpring();
-				initDB();
-
-				String benchmarkCriteria = ApplicationContext
-						.getBenchmarkConfig().getBenchmarkCriteria();
-				String warmupCriteria = ApplicationContext.getBenchmarkConfig()
-						.getWarmupCriteria();
-				String initBookCriteria = ApplicationContext
-						.getBenchmarkConfig().getInitBookCriteria();
-				int messageRate = ApplicationContext.getBenchmarkConfig()
-						.getMessageRate();
-				int warmUpMessages = ApplicationContext.getBenchmarkConfig()
-						.getWarmUpMessages();
-				int warmUpPause = ApplicationContext.getBenchmarkConfig()
-						.getWarmUpPause();
-				int benchmarkSymbolList = ApplicationContext
-						.getBenchmarkConfig().getBenchmarkSymbolList();
-				int securityClass = ApplicationContext.getBenchmarkConfig()
-						.getSecurityClass();
-
-				List<String> benchmarkTestIDList = DBUtil.getInstance()
-						.getTcm().getTestCaseList(benchmarkCriteria);
-				List<String> warmupTestIDList = DBUtil.getInstance().getTcm()
-						.getTestCaseList(warmupCriteria);
-				List<String> initBookTestIDList = DBUtil.getInstance().getTcm()
-						.getTestCaseList(initBookCriteria);
-
-				String symbolPrefix = ApplicationContext.getConfig()
-						.getDefaultSymbolMap().get(securityClass);
-
-				AutoPilotBenchMarkSuite.initBookPhase(benchmarkSymbolList,
-						symbolPrefix, initBookTestIDList);
-
-				AutoPilotBenchMarkSuite.warmupPhase(benchmarkSymbolList,
-						warmUpMessages, symbolPrefix, warmupTestIDList);
-
-				Thread.sleep(warmUpPause);
-
-				AutoPilotBenchMarkSuite.benchmarkPhase(benchmarkSymbolList,
-						messageRate, symbolPrefix, benchmarkTestIDList);
-
-				shutdownAutoPilot();
-			} else {
-				logger.info("Launch AutoPilot at Servermode...");
-
-				initSpring();
-				initDB();
-				
-				// to add a testcase list based on a query
-				String strLabel = ApplicationContext.getConfig()
-						.getServerModeTestCaseQuery();
-				
-				String strReleaseNum = ApplicationContext.getConfig()
-						.getReleaseNum();
-				
-				strLabel = checkNullStrValue(strLabel);
-				strReleaseNum = checkNullStrValue(strReleaseNum);
-				
-				List<String> tcIDList = null;
-				
-				//if only release number given 
-				if((!strReleaseNum.equals("")) && (strLabel.equals(""))){
-					tcIDList = DBUtil.getInstance().getTcm()
-							.getTestCaseList(strReleaseNum);
-					
-					// if only label given 
-				}else if ((strReleaseNum.equals("")) && (!strLabel.equals(""))){
-					tcIDList = DBUtil.getInstance().getTcm()
-							.getTestCaseListByLabel(strLabel);
-										
-					// both release and label given populate 2 lists and retain common elements  
-				}else{
-					tcIDList = DBUtil.getInstance().getTcm()
-							.getTestCaseList(strReleaseNum);
-					
-					List<String> labelList = DBUtil.getInstance().getTcm()
-							.getTestCaseListByLabel(strLabel);
-					
-					tcIDList.retainAll(labelList);
-				}
-				
-				int intPassed = 0, intFailed = 0;
-				List<String> failedTestcases = new ArrayList<String>();
-				List<String> passedTestcases = new ArrayList<String>();
-				logger.info("***************************AutoPilot ServerMode Run Started. Total TestCase:"
-						+ tcIDList.size());
-				logger.info("TESTRESULTLOG|AutoPilot ServerMode Run Started. ");
-				logger.info("TESTRESULTLOG|Total # of TestCases: "
-						+ tcIDList.size());
-				logger.info("Criteria:: Label: " + strLabel+", Release No: "+strReleaseNum);
-				for (String strTcID : tcIDList) {
-					try {
-						/*
-						 * forcely turn off the debug for the server mode run
-						 */
-						ApplicationContext.getConfig().setDebug(false);
-						ValidationObject validationObject = TestCaseController.INSTANCE
-								.runPersistedTestCase(strTcID, "", false);
-
-						/*
-						 * ValidationObject voTemp =
-						 * ResultedOutputController.getInstance
-						 * ().getResultedOutputObj(strTcID);
-						 * 
-						 * String strXml =voTemp.toXmlString();
-						 * logger.info(strXml);
-						 * 
-						 * AutoPilotValidationResultParser aParser = new
-						 * AutoPilotValidationResultParser(strXml);
-						 * ValidationObject voTempBack = aParser.getVObject();
-						 * 
-						 * voTempBack.printValidationObject();
-						 */
-
-						if (validationObject.isSuccess()) {
-							intPassed++;
-							passedTestcases.add(strTcID);
-							logger.info("****************TestCase " + strTcID
-									+ ": PASSED.");
-						}
-
-						else {
-							intFailed++;
-							failedTestcases.add(strTcID);
-							logger.info("****************TestCase " + strTcID
-									+ ": FAILED.");
-						}
-
-						Thread.sleep(1000);
-
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-				}
-
-				logger.info("TESTRESULTLOG|AutoPilot ServerMode Run Finished.");
-				logger.info("TESTRESULTLOG|PASSED:" + intPassed + " FAILED:"
-						+ intFailed);
-				logger.info("TESTRESULTLOG|List of passed test cases:  ");
-				logger.info("TESTRESULTLOG|" + passedTestcases.toString());
-				logger.info("TESTRESULTLOG|List of failed test cases:  ");
-				logger.info("TESTRESULTLOG|" + failedTestcases.toString());
-
-				// cleanUpAutoPilot();
-				shutdownAutoPilot();
+				break;
+			case "benchmarkmode":
+				launchBenchmarkMode();
+				break;
+			default:
+				launchServerMode();
+				break;
 			}
 
 		} catch (Exception ex) {
@@ -202,9 +60,155 @@ public class AutoPilotBootstrap {
 		}
 
 	}
-	
+
+	private static void launchServerMode() throws Exception {
+		loadClasspath();
+		initSpring();
+		initDB();
+		List<String> labels = new ArrayList<>();
+		List<String> releases = new ArrayList<>();
+		String strLabel;
+		String strReleaseNum;
+		if(System.getProperty("testCaseLabels") != null){
+			labels = Arrays.asList(System.getProperty("testCaseLabels").split(","));
+		}
+
+		if(null != System.getProperty("releases")){
+			releases = Arrays.asList(System.getProperty("releases").split(","));
+		}
+
+		if(labels.isEmpty()) {
+			strLabel = checkNullStrValue(ApplicationContext.getConfig().getServerModeTestCaseQuery());
+			if("" != strLabel) {
+				labels.add(strLabel);
+			}
+		}
+
+		if(releases.isEmpty()) {
+			strReleaseNum = checkNullStrValue(ApplicationContext.getConfig().getReleaseNum());
+			if("" != strReleaseNum ){
+				releases.add(strReleaseNum);
+			}
+		}
+
+		List<String> tcIDList = new ArrayList<>();
+
+
+		if((!releases.isEmpty()) && (labels.isEmpty())){
+			//if only release number given 
+			releases.forEach(addReleaseToList(tcIDList));
+		}else if ((releases.isEmpty()) && (!labels.isEmpty())){
+			// if only label given 
+			System.out.println("IN HERE");
+			labels.forEach(addLabelToList(tcIDList));
+		}else{
+			// both release and label given populate 2 lists and retain common elements  
+			releases.forEach(addReleaseToList(tcIDList));
+			List<String> labelList = new ArrayList<>();
+			labels.forEach(label -> labelList.addAll(DBUtil.getInstance().getTcm().getTestCaseListByLabel(label)));
+
+			tcIDList.retainAll(labelList);
+		}
+
+		int intPassed = 0, intFailed = 0;
+		List<String> failedTestcases = new ArrayList<String>();
+		List<String> passedTestcases = new ArrayList<String>();
+		System.out.println("***************************AutoPilot ServerMode Run Started. Total TestCase:"
+				+ tcIDList.size());
+		System.out.println("TESTRESULTLOG|AutoPilot ServerMode Run Started. ");
+		System.out.println("TESTRESULTLOG|Total # of TestCases: "
+				+ tcIDList.size());
+		System.out.println("Criteria:: Labels: " + labels.toString()+", Releases: "+releases.toString());
+		for (String strTcID : tcIDList) {
+			try {
+				/*
+				 * forcely turn off the debug for the server mode run
+				 */
+				ApplicationContext.getConfig().setDebug(false);
+				ValidationObject validationObject = TestCaseController.INSTANCE.runPersistedTestCase(strTcID, "", false);
+
+				if (validationObject.isSuccess()) {
+					intPassed++;
+					passedTestcases.add(strTcID);
+					System.out.println("****************TestCase " + strTcID+ ": PASSED.");
+				}else {
+					intFailed++;
+					failedTestcases.add(strTcID);
+					System.out.println("****************TestCase " + strTcID+ ": FAILED.");
+				}
+
+				Thread.sleep(1000);
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		System.out.println("TESTRESULTLOG|AutoPilot ServerMode Run Finished.");
+		System.out.println("TESTRESULTLOG|PASSED:" + intPassed + " FAILED:"
+				+ intFailed);
+		System.out.println("TESTRESULTLOG|List of passed test cases:  ");
+		System.out.println("TESTRESULTLOG|" + passedTestcases.toString());
+		System.out.println("TESTRESULTLOG|List of failed test cases:  ");
+		System.out.println("TESTRESULTLOG|" + failedTestcases.toString());
+
+		// cleanUpAutoPilot();
+		shutdownAutoPilot();
+	}
+
+	private static Consumer<? super String> addLabelToList(List<String> tcIDList) {
+		return label -> tcIDList.addAll(DBUtil.getInstance().getTcm().getTestCaseListByLabel(label));
+	}
+
+	private static Consumer<? super String> addReleaseToList(List<String> tcIDList) {
+		return release -> tcIDList.addAll(DBUtil.getInstance().getTcm().getTestCaseList(release));
+	}
+
+	private static void launchBenchmarkMode() throws Exception, InterruptedException {
+		loadClasspath();
+		initSpring();
+		initDB();
+
+		String benchmarkCriteria = ApplicationContext.getBenchmarkConfig().getBenchmarkCriteria();
+		String warmupCriteria = ApplicationContext.getBenchmarkConfig().getWarmupCriteria();
+		String initBookCriteria = ApplicationContext.getBenchmarkConfig().getInitBookCriteria();
+		int messageRate = ApplicationContext.getBenchmarkConfig().getMessageRate();
+		int warmUpMessages = ApplicationContext.getBenchmarkConfig().getWarmUpMessages();
+		int warmUpPause = ApplicationContext.getBenchmarkConfig().getWarmUpPause();
+		int benchmarkSymbolList = ApplicationContext.getBenchmarkConfig().getBenchmarkSymbolList();
+		int securityClass = ApplicationContext.getBenchmarkConfig().getSecurityClass();
+
+		List<String> benchmarkTestIDList = DBUtil.getInstance().getTcm().getTestCaseList(benchmarkCriteria);
+		List<String> warmupTestIDList = DBUtil.getInstance().getTcm().getTestCaseList(warmupCriteria);
+		List<String> initBookTestIDList = DBUtil.getInstance().getTcm().getTestCaseList(initBookCriteria);
+
+		String symbolPrefix = ApplicationContext.getConfig().getDefaultSymbolMap().get(securityClass);
+
+		AutoPilotBenchMarkSuite.initBookPhase(benchmarkSymbolList,symbolPrefix, initBookTestIDList);
+
+		AutoPilotBenchMarkSuite.warmupPhase(benchmarkSymbolList,warmUpMessages, symbolPrefix, warmupTestIDList);
+
+		Thread.sleep(warmUpPause);
+
+		AutoPilotBenchMarkSuite.benchmarkPhase(benchmarkSymbolList,messageRate, symbolPrefix, benchmarkTestIDList);
+
+		shutdownAutoPilot();
+	}
+
+	private static void loadClasspath() throws Exception {
+		String strConfighome =  System.getProperty("config.home");
+		String strCommon =  System.getProperty("common");
+
+		String strClassPathRegion = strConfighome + "/" + System.getProperty("application")+"/" + System.getProperty("region");
+		String strClassPathCommon = strClassPathRegion+"/" + strCommon;
+		String strClassPathDb = strClassPathCommon + "/" + "db";
+		loadClassPath(strClassPathRegion);
+		loadClassPath(strClassPathCommon);
+		loadClassPath(strClassPathDb);
+	}
+
 	private static String checkNullStrValue(String strValue){
-		
+
 		if (strValue == null || strValue.trim().isEmpty()){
 			strValue = "";
 		}
